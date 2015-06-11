4f9849e231418782a8c7aa05068f706077da8966
griffinmacias <griffinmacias@gmail.com>
2015-06-11 14:34:19 -0400

    Passed all Blackjack Tests

---
+++ objc-BlackJackViews/Classes/FISBlackjackGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISBlackjackGameViewController.h
+//  objc-BlackJackViews
+//
+//  Created by Mason Macias on 6/10/15.
+//  Copyright (c) 2015 Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISBlackjackGameViewController : UIViewController
+
+@end
